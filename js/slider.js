2c04d0bb8597d42940b0bc6da67ea02dc9de1948
AliHoushyar <alihkhoshdell@gmail.com>
2024-02-08 08:27:40 +0330

    make page responsive

--- js/slider.js
+++ js/slider.js
@@ -1,19 +1,19 @@
-let slideIndex = 1;
-showSlides(1);
+// let slideIndex = 1;
+// showSlides(1);
 
-function showSlides(n) {
-    let i;
-    let imgb = document.getElementById("myimg");
-    if(n>2)
-        n=n%2 + 1;
-    switch (n){
-        case 1:
-            imgb.style.backgroundImage = "url(./img/hotel/main.jpg)";
-            document.getElementById('airplain-btn').src = "./icons/plain-logo-2.png";
-            break;
-        case 2:
-            imgb.style.backgroundImage = "url('./img/holy.jpg')";
-            document.getElementById('hotel-btn').src = "./icons/hotel-logo-2.png";
-            break;
-    }
-}
+// function showSlides(n) {
+//     let i;
+//     let imgb = document.getElementById("myimg");
+//     if(n>2)
+//         n=n%2 + 1;
+//     switch (n){
+//         case 1:
+//             imgb.style.backgroundImage = "url(./img/hotel/main.jpg)";
+//             document.getElementById('airplain-btn').src = "./icons/plain-logo-2.png";
+//             break;
+//         case 2:
+//             imgb.style.backgroundImage = "url('./img/holy.jpg')";
+//             document.getElementById('hotel-btn').src = "./icons/hotel-logo-2.png";
+//             break;
+//     }
+// }
